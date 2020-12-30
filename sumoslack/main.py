e8b848253fecbe305e43c6abc344aabe4d1a4da6
sourabh <sourabh@sumologic.com>
2020-12-30 16:05:55 +0530

    removing run method as it is called by BaseCollector class

--- sumoslack/main.py
+++ sumoslack/main.py
@@ -5,7 +5,6 @@ from random import shuffle
 
 sys.path.insert(0, '/opt')  # layer packages are in opt directory
 
-from concurrent import futures
 from slackclient import SlackClient
 from sumoappclient.common.utils import get_current_timestamp
 from sumoappclient.sumoclient.base import BaseCollector
@@ -61,14 +60,6 @@ class SumoSlackCollector(BaseCollector):
         else:
             self.log.error("Team name call failed with error as %s", data["error"])
             sys.exit()
-
-    def is_running(self):
-        self.log.debug("Acquiring single instance lock")
-        return self.kvstore.acquire_lock(self.SINGLE_PROCESS_LOCK_KEY)
-
-    def stop_running(self):
-        self.log.debug("Releasing single instance lock")
-        return self.kvstore.release_lock(self.SINGLE_PROCESS_LOCK_KEY)
 
     def build_task_params(self):
         self.log.info("Building task Parameters............")
@@ -199,31 +190,6 @@ class SumoSlackCollector(BaseCollector):
             self.log.error("Error Occurred while fetching Audit Actions Error %s", exc)
         finally:
             sess.close()
-
-    def run(self, *args, **kwargs):
-        if self.is_running():
-            try:
-                self.log.info('Starting Slack Sumo Collector...')
-                # task_params = self.build_task_params()
-                all_futures = {}
-                self.log.debug("spawning %d workers" % self.config['Collection']['NUM_WORKERS'])
-                with futures.ThreadPoolExecutor(max_workers=self.config['Collection']['NUM_WORKERS']) as executor:
-                    results = {executor.submit(apiobj.fetch): apiobj for apiobj in self.task_params}
-                    all_futures.update(results)
-                for future in futures.as_completed(all_futures):
-                    param = all_futures[future]
-                    api_type = str(param)
-                    try:
-                        future.result()
-                        obj = self.kvstore.get(api_type)
-                    except Exception as exc:
-                        self.log.error(f"API Type: {api_type} thread generated an exception: {exc}", exc_info=True)
-                    else:
-                        self.log.info(f"API Type: {api_type} thread completed {obj}")
-            finally:
-                self.stop_running()
-        else:
-            self.kvstore.release_lock_on_expired_key(self.SINGLE_PROCESS_LOCK_KEY, expiry_min=10)
 
 
 def main(*args, **kwargs):
